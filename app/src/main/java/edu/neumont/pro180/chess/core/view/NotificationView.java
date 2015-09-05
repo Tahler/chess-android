9cc3730f8aed4cbf9842213d301925fcea25802e
John Anglin <anglicarver@gmail.com>
2015-09-04 20:37:33 -0600

    Skeletons of UI components:

---
+++ app/src/main/java/edu/neumont/pro180/chess/core/view/NotificationView.java
@@ -0,0 +1,11 @@
+package edu.neumont.pro180.chess.core.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public class NotificationView extends LinearLayout {
+    public NotificationView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
